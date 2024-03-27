a249f557451668cecc0e9f5892f8d5872203e082
Mohit Yadav <70257688+yadavmoh09@users.noreply.github.com>
2024-03-27 13:54:33 +0530

    addes JS Files

---
+++ js/data.js
@@ -0,0 +1,60 @@
+var dataProject = [
+  {
+    "project_name": "Document Verification Engine",
+    "role": "Software engineer",
+    "tools":"Excel, Outlook, OCR, VB.net",
+    "project_description": "Using OCR (Optical Character Recognition) technology. This system intelligently cross-references documents such as Aadhar cards, PAN cards, cheques, and mark sheets, ensuring a remarkable 98% accuracy rate in identity"
+  },
+  {
+    "project_name": "Chalhoub Group – UAE",
+    "role": "Software engineer",
+    "tools":"Excel, Outlook, OCR, Document Understanding Framwork, VB.net",
+    "project_description": "Leveraging an array of tools, including Excel, Outlook, web browsers (Chrome and IE), Abbey, Java, UiPath, API Integration, AI Center, and Data Services, achieved remarkable results. Notably, streamlined operations led to a 30% reduction in manual efforts, a 20% decrease in error rates, and cost savings of $500,000 annually"
+  },
+  {
+    "project_name": "Amara Raja Group - India",
+    "role": "Software engineer",
+    "tools":"Excel, Outlook, Document Understanding Framwork, SAP, VB.net",
+    "project_description": "Automated finance processes at Amararaja Group using MS-Excel, MS-Outlook, SAP, Document Understanding, AI Center, and Action Center for increased efficiency and accuracy"
+  },
+  {
+    "project_name": "Menabev - KSA",
+    "role": "Software engineer",
+    "tools":"Excel, Outlook, SAP, Chrome, VB.net",
+    "project_description": "I've designed and implemented innovative automation solutions in finance and HR, driving efficiency, accuracy, and cost savings. My work has transformed complex processes into streamlined systems, contributing to Menabev's success."
+  },
+  {
+    "project_name": "Alsagr Insurance - KSA",
+    "role": "Software engineer",
+    "tools":"Excel, Outlook, Java Based Application, Chrome, VB.net",
+    "project_description": "As an RPA Developer at Alsagr Insurance, I led the implementation of robotic process automation solutions for finance and claims processes. Collaborating closely with stakeholders, I analyzed existing workflows and designed tailored RPA solutions to streamline operations. Leveraging leading RPA platforms, I developed and deployed bots to automate repetitive tasks, resulting in significant efficiency gains and cost savings. By optimizing processes and enhancing accuracy, our RPA initiatives improved transaction processing speed and customer service while empowering employees to focus on strategic initiatives. Overall, our efforts drove transformative changes, delivering tangible benefits in terms of productivity, accuracy, and resource optimization for Alsagr Insurance."
+  },
+  {
+    "project_name": "Theeb - KSA",
+    "role": "Software engineer",
+    "tools":"Excel, Outlook, Chrome, VB.net",
+    "project_description": "At Theeb, I served as an RPA Developer, driving automation initiatives to optimize operations. Working closely with cross-functional teams, I designed and implemented RPA solutions to streamline processes across various departments. Leveraging leading RPA tools such as UiPath or Automation Anywhere, I developed bots to automate repetitive tasks, enhance efficiency, and reduce manual errors. By integrating RPA technology into workflows, we achieved notable improvements in productivity and accuracy, contributing to Theeb's strategic goals of innovation and operational excelle"
+  }
+];
+
+var dataExperience = [
+  {
+    "role": "Automation engineer",
+    "working_period": "Oct-2021 To present",
+    "company":"Novigo solutions",
+    "company_location":"Bangluru, KA, India"
+  }
+];
+
+var dataEducation = [
+  {
+    "course": "Bachelor's of Computer Application",
+    "period": "2017 - 2020",
+    "collage":"Holkar science collage, Indore, Madhya pradesh"
+  },
+  {
+    "course": "Master's Of Computer Application",
+    "period": "2020 - 2022",
+    "collage":"SAGE University, Indore, Madhya pradesh"
+  }
+];
---
+++ js/main.js
@@ -0,0 +1,73 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Navbar on scrolling
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.navbar').fadeIn('slow').css('display', 'flex');
+        } else {
+            $('.navbar').fadeOut('slow').css('display', 'none');
+        }
+    });
+
+
+    // Smooth scrolling on the navbar links
+    $(".navbar-nav a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top - 45
+            }, 1500, 'easeInOutExpo');
+            
+            if ($(this).parents('.navbar-nav').length) {
+                $('.navbar-nav .active').removeClass('active');
+                $(this).closest('a').addClass('active');
+            }
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+    
+
+    // Typed Initiate
+    if ($('.typed-text-output').length == 1) {
+        var typed_strings = $('.typed-text').text();
+        var typed = new Typed('.typed-text-output', {
+            strings: typed_strings.split(', '),
+            typeSpeed: 100,
+            backSpeed: 20,
+            smartBackspace: false,
+            loop: true
+        });
+    }
+    
+})(jQuery);
+
